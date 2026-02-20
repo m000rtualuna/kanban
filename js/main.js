8877ac982671197378ff811d7d90eff51d0d3327
diana <m000rtualuna@gmail.com>
2026-02-20 12:20:42 +0700

    added task editing

--- js/main.js
+++ js/main.js
@@ -1,16 +1,52 @@
 Vue.component('task', {
+    props: ['task', 'isEditable'],
+
     data() {
         return {
+            editing: false,
+            editTitle: this.task.title,
         };
+    },
+
+    methods: {
+        startEdit() {
+            if (this.isEditable) {
+                this.editing = true;
+                this.editTitle = this.task.title;
+            }
+        },
+
+        saveEdit() {
+            if (this.editTitle.trim()) {
+                this.$emit('edit-task', this.task.id, this.editTitle.trim());
+                this.editing = false;
+            }
+        },
+        cancelEdit() {
+            this.editing = false;
+        },
     },
 
     template: `
     <div class="task">
+    <div v-if="!editing">
+    <span>{{ task.title }}</span>
+    <template v-if="isEditable">
+    <button @click="startEdit">редактировать</button>
+</template>
+    <div v-else>
+    <input v-model="editTitle" @keyup.enter="saveEdit">
+    <button @click="saveEdit">сохранить</button>
+    <button @click="cancelEdit">отмена</button>
+</div>
+</div>
     </div>
   `
 });
 
 Vue.component('column', {
+    props: ['tasks'],
+
     data() {
         return {
             newTaskTitle:''
@@ -23,6 +59,10 @@ Vue.component('column', {
                 this.$emit('add-task', title);
                 this.newTaskTitle = '';
             }
+        },
+
+        editTask(id, newTitle) {
+            this.$emit('edit-task', id, newTitle);
         }
     },
 
@@ -30,10 +70,11 @@ Vue.component('column', {
     <div class="column">
      
       <div class="add-task">
-        <input placeholder="Новая задача" v-model="newTaskTitle" @keyup.enter="addTask" />
+        <input placeholder="Новая задача" v-model="newTaskTitle" @keyup.enter="addTask"/>
         <button @click="addTask">Добавить</button>
       </div>
       
+      <task v-for="task in tasks" :task="task" @edit-task=editTask"></task>
     </div>
   `
 });
@@ -53,6 +94,10 @@ new Vue({
     methods: {
         addTask(title) {
             this.tasks.push({id: this.nextId++, title, status: this.statuses[0] })
+        },
+
+        editTask(id, newTitle) {
+            const t = this.tasks.find(t => t.id === id);
         }
 
     },
